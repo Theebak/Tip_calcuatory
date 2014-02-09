85ee12102f613e40368e09235dff2bc1e0ff48a8
Theebak <theebak.sothilingam@gmail.com>
2014-02-08 19:22:06 -0500

    Version 3.0

--- tip_calculator.py
+++ tip_calculator.py
@@ -3,9 +3,9 @@
 
 
 
-meal = float(raw_input ("Enter the cost of the meal"))
-tax = float(raw_input ("Enter the tax percentage"))
-tip = float(raw_input ("Enter the tip percentage"))
+meal = float(raw_input ("Enter the cost of the meal "))
+tax = float(raw_input ("Enter the tax percentage "))/100
+tip = float(raw_input ("Enter the tip percentage "))/100
 
 tax_value = meal * tax
 meal_with_tax = meal + tax_value
@@ -13,10 +13,10 @@ tip_value = meal * tip
 
 
 total = meal_with_tax + tip_value
-print("%.2f" % total)
+	
 
 
-print ('The base cost of the meal is {:.2f}'.format(meal))
-print ('The dollar value of the tax on the meal is {:.2f}'.format(tax_value))
-print ('The Tip that they will need to pay is {:.2f}'.format(tip_value))
-print ('The grand total for the meal, inclusive of tax and tip is {:.2f}'.format (total))
+print ('The base cost of the meal is ${:.2f}'.format(meal))
+print ('The dollar value of the tax on the meal is ${:.2f}'.format(tax_value))
+print ('The Tip that they will need to pay is ${:.2f}'.format(tip_value))
+print ('The grand total for the meal, inclusive of tax and tip is ${:.2f}'.format (total))
